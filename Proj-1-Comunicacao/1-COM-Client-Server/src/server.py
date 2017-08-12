296cc0bb7afa9307978b0eb588d50d40fb807041
Martim Ferreira <martim.ferreira.jose@gmail.com>
2017-08-12 20:11:06 -0300

    ideia inicial do servirdor

--- Proj-1-Comunicacao/1-COM-Client-Server/src/server.py
+++ Proj-1-Comunicacao/1-COM-Client-Server/src/server.py
@@ -1,4 +1,4 @@
-#!/usr/bi+n/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #####################################################
 # Camada Física da Computação
@@ -10,7 +10,6 @@
 from enlace import *
 import time
 
-
 # Serial Com Port
 #   para saber a sua porta, execute no terminal :
 #   python -m serial.tools.list_ports
@@ -19,27 +18,15 @@ import time
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
 serialName = "COM4"                  # Windows(variacao de)
 
-# contador main
-contador = 0
-
-def main(texto):
-    global contador
-    contador+= 1
-
+def main():
     # Inicializa enlace
     com = enlace(serialName)
 
     # Ativa comunicacao
     com.enable()
 
-    # Endereco da imagem a ser transmitida
-    imageR = texto
-
     # Endereco da imagem a ser salva
-    stringA = "./imgs/recebida"
-    stringB = str(contador)
-    stringC = ".png"
-    imageW = stringA + stringB + stringC 
+    imageW = "./imgs/recebida.png"
 
     # Log
     print("-------------------------")
@@ -47,25 +34,19 @@ def main(texto):
     print("  porta : {}".format(com.fisica.name))
     print("-------------------------")
 
-    # Carrega imagem
-    print ("Carregando imagem para transmissão :")
-    print (" - {}".format(imageR))
+    # Faz a recepção dos dados
+    print ("Recebendo dados .... ")
+    rxBuffer, nRx = com.getData(txLen)
+
+    # log
+    print ("Lido              {} bytes ".format(nRx))
+
+    # Salva imagem recebida em arquivo
     print("-------------------------")
-    txBuffer = open(imageR, 'rb').read()
-    txLen    = len(txBuffer)
-    print(txLen)
-
-    # Transmite imagem
-    print("Transmitindo .... {} bytes".format(txLen))
-    com.sendData(txBuffer)
-
-    # espera o fim da transmissão
-    while(com.tx.getIsBussy()):
-        pass
-
-    # Atualiza dados da transmissão
-    txSize = com.tx.getStatus()
-    print ("Transmitido       {} bytes ".format(txSize))
+    print ("Salvando dados no arquivo :")
+    print (" - {}".format(imageW))
+    f = open(imageW, 'wb')
+    f.write(rxBuffer)
 
     # Fecha arquivo de imagem
     f.close()
