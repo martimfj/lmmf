e5e0088fe8a390fcee6c064f2625f245d0bdbf92
Leostayner <leostayner@gmail.com>
2017-08-12 16:49:14 -0300

    Interface pronta

---
+++ Proj-1-Comunicacao-no-repositorio/src/client.py
@@ -0,0 +1,94 @@
+#!/usr/bi+n/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+# Prof. Rafael Corsi
+#  Abril/2017
+#  Aplicação
+####################################################
+
+from enlace import *
+import time
+
+
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+
+serialName = "COM4"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+#serialName = "COM3"                  # Windows(variacao de)
+
+# contador main
+contador = 0
+
+def main(texto):
+    global contador
+    contador+= 1
+
+    # Inicializa enlace
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+    # Endereco da imagem a ser transmitida
+    imageR = texto
+
+    # Endereco da imagem a ser salva
+    stringA = "./imgs/recebida"
+    stringB = str(contador)
+    stringC = ".png"
+    imageW = stringA + stringB + stringC 
+
+    # Log
+    print("-------------------------")
+    print("Comunicação inicializada")
+    print("  porta : {}".format(com.fisica.name))
+    print("-------------------------")
+
+    # Carrega imagem
+    print ("Carregando imagem para transmissão :")
+    print (" - {}".format(imageR))
+    print("-------------------------")
+    txBuffer = open(imageR, 'rb').read()
+    txLen    = len(txBuffer)
+    print(txLen)
+
+    # Transmite imagem
+    print("Transmitindo .... {} bytes".format(txLen))
+    com.sendData(txBuffer)
+
+    # espera o fim da transmissão
+    while(com.tx.getIsBussy()):
+        pass
+
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+    print ("Transmitido       {} bytes ".format(txSize))
+
+    # Faz a recepção dos dados
+    print ("Recebendo dados .... ")
+    rxBuffer, nRx = com.getData(txLen)
+
+    # log
+    print ("Lido              {} bytes ".format(nRx))
+
+    # Salva imagem recebida em arquivo
+    print("-------------------------")
+    print ("Salvando dados no arquivo :")
+    print (" - {}".format(imageW))
+    f = open(imageW, 'wb')
+    f.write(rxBuffer)
+
+    # Fecha arquivo de imagem
+    f.close()
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+if __name__ == "__main__":
+    main()
